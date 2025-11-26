ef651b67e040c61c0d05749d25ee58b0cbcaf9c7
Florian <florian.caplet@Florians-MacBook-Air.local>
2025-11-26 10:50:04 +0100

    Feat : Structure HTML

--- 003-php-password-generator/app/index.php
+++ 003-php-password-generator/app/index.php
@@ -1,3 +1,23 @@
 <?php
 
-echo "Hello world";
+
+
+
+
+$html = <<<HTML
+<!doctype html>
+<html lang=fr>
+<head>
+<meta charset="UTF-8">
+             <meta name="viewport" content="width=device-width, initial-scale=1">
+             <title>Générateur de mots de passe</title>
+</head>
+<body>
+  <h1>Générateur de mots de passe</h1>
+  <form>
+  </form>
+</body>
+</html>
+HTML;
+
+echo $html;
